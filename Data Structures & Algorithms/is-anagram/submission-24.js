e4c139bf9e41420e69a6cdde22048b6853cb5e3d
Andrew <102989101+andredolgoleo@users.noreply.github.com>
2026-08-08 01:57:50 +0200

    Add: is-anagram - submission-24

---
+++ Data Structures & Algorithms/is-anagram/submission-24.js
@@ -0,0 +1,37 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if (s.length !== t.length) {
+            return false;
+        }
+
+        const hashMap = new Map();
+
+        for (const letter of s) {
+            if (hashMap.has(letter)) {
+                const value = hashMap.get(letter);
+                hashMap.set(letter, value + 1);
+                continue;
+            }
+
+            hashMap.set(letter, 1);
+        }
+
+        for (const letter of t) {
+            if (!hashMap.has(letter) || hashMap.get(letter) === 0) {
+                return false;
+            }
+
+            if (hashMap.has(letter)) {
+                const value = hashMap.get(letter);
+                hashMap.set(letter, value - 1);
+            }
+        }
+
+        return true;
+    }
+}
