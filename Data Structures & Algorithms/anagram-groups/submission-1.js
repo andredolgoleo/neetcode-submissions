ab3dda888028ea18a09ed392bd115dc3ff886f9f
Andrew <102989101+andredolgoleo@users.noreply.github.com>
2026-08-08 06:28:21 +0200

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        const hashTable = new Map();
+
+        for (let i = 0; i < strs.length; i++) {
+            const str = strs[i];
+            const sortedStr = str.split("").sort().join("");
+
+            if (hashTable.has(sortedStr)) {
+                hashTable.set(sortedStr, [...hashTable.get(sortedStr), str]);
+                continue;
+            }
+
+            hashTable.set(sortedStr, [str]);
+        }
+
+        return Array.from(hashTable.values());
+    }
+}
