b91f19cb75e1ea884590f4bc1ca9b352af09eb2f
Andrew <102989101+andredolgoleo@users.noreply.github.com>
2026-08-09 21:57:25 +0200

    Add: top-k-elements-in-list - submission-3

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-3.ts
@@ -0,0 +1,69 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+
+    insertionSort(nums) {
+        for (let i = 0; i < nums.length; i++) {
+            let currentPointer = i;
+
+            while (currentPointer && nums[currentPointer - 1] >= nums[currentPointer]) {
+                [nums[currentPointer], nums[currentPointer - 1]] = [
+                    nums[currentPointer - 1],
+                    nums[currentPointer],
+                ];
+
+                currentPointer -= 1;
+            }
+        }
+
+        return nums;
+    }
+
+    topKFrequent(nums: number[], k: number): number[] {
+        const buckets = new Map(
+            Array.from(Array(nums.length + 1), (_, idx) => {
+                return [idx, []];
+            }),
+        );
+
+        const frequenciesCount = new Map();
+
+        for (const num of nums) {
+            if (frequenciesCount.has(num)) {
+                frequenciesCount.set(num, frequenciesCount.get(num) + 1);
+                continue;
+            }
+
+            frequenciesCount.set(num, 1);
+        }
+
+        for (const [key, value] of frequenciesCount) {
+            const bucketContent = buckets.get(Math.floor(value));
+
+            bucketContent.push(key);
+        }
+
+        const result = [];
+
+        for (let i = buckets.size - 1; i >= 0; i--) {
+            const value = buckets.get(i);
+
+            if (value.length < 1) {
+                continue;
+            }
+
+            for (let j = value.length - 1; j >= 0; j--) {
+ result.push(value[j]);
+
+                if (result.length === k) {
+                    return result;
+                }
+
+               
+            }
+        }
+    }
+}
