63e1c5e6a56486a73ebf78cd65d12aca8a61b7fa
Andrew Dolhov <102989101+andredolgoleo@users.noreply.github.com>
2026-08-20 22:15:53 +0200

    Add: is-anagram - submission-30

---
+++ Data Structures & Algorithms/is-anagram/submission-30.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        hashMapS = {}
+        hashMapT = {}
+
+        for _, c in enumerate(s):
+            if hashMapS.get(c):
+                hashMapS[c] += 1
+                continue
+            hashMapS[c] = 1
+        
+        for _, c in enumerate(t):
+            if hashMapT.get(c):
+                hashMapT[c] += 1
+                continue
+            hashMapT[c] = 1
+
+        return hashMapS == hashMapT
